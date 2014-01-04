f6882b15c656a8d69d7091c750d872e1f9176a68
kp001 <karunesh.ind@gmail.com>
2014-01-04 18:46:56 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,41 @@
+
 #include<iostream>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <vector>
+#include <iostream>
+#include "CCnetproxy.h"
+//#include "thread.h"
+
+using namespace std;
+
+
+void* start_thread_KP(void* arg)
+{
+	
+  return NULL;
+}
+
+
+int main(int argc,char *argv[])
+{
+	int ret=0;
+	CCNetProxy cd;
+	
+	cd.build_tp();
+
+	
+while(ret<15)
+{
+	sleep(1);
+	ret++;
+
+}
+
+cout<<"\nApp Exiting\n";
+return 0;
+}
+
+
